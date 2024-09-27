308d9a2e38d36790427aff4116efd68f1be70ca7
bdw429s <bdw429s@users.noreply.github.com>
2024-09-27 04:50:06 +0000

    Apply cfformat changes

--- src/main/java/ortus/boxlang/web/handlers/WebsocketHandler.java
+++ src/main/java/ortus/boxlang/web/handlers/WebsocketHandler.java
@@ -15,56 +15,56 @@ import ortus.boxlang.web.MiniServer;
 
 public class WebsocketHandler extends PathHandler {
 
-    private final HttpHandler                       next;
-    private final Set<WebSocketChannel>             connections = Collections.synchronizedSet( new HashSet<>() );
-    private final WebSocketProtocolHandshakeHandler webSocketProtocolHandshakeHandler;
+	private final HttpHandler						next;
+	private final Set<WebSocketChannel>				connections	= Collections.synchronizedSet( new HashSet<>() );
+	private final WebSocketProtocolHandshakeHandler	webSocketProtocolHandshakeHandler;
 
-    public WebsocketHandler( final HttpHandler next, String prefixPath ) {
-        super( next );
-        this.next                              = next;
-        this.webSocketProtocolHandshakeHandler = new WebSocketProtocolHandshakeHandler(
-            new WebSocketConnectionCallback() {
+	public WebsocketHandler( final HttpHandler next, String prefixPath ) {
+		super( next );
+		this.next								= next;
+		this.webSocketProtocolHandshakeHandler	= new WebSocketProtocolHandshakeHandler(
+		    new WebSocketConnectionCallback() {
 
-                @Override
-                public void onConnect( WebSocketHttpExchange WSexchange, WebSocketChannel channel ) {
-                    // Add the new channel to the set of connections
-                    connections.add( channel );
-                    WebsocketReceiveListener listener = new WebsocketReceiveListener( MiniServer.getCurrentExchange(),
-                        next,
-                        channel );
-                    channel.getReceiveSetter().set( listener );
-                    channel.getCloseSetter().set( ( c ) -> {
-                        connections.remove( channel );
-                        listener.onClose( c );
-                    } );
-                    channel.resumeReceives();
-                }
-            }, next );
+			    @Override
+			    public void onConnect( WebSocketHttpExchange WSexchange, WebSocketChannel channel ) {
+				    // Add the new channel to the set of connections
+				    connections.add( channel );
+				    WebsocketReceiveListener listener = new WebsocketReceiveListener( MiniServer.getCurrentExchange(),
+				        next,
+				        channel );
+				    channel.getReceiveSetter().set( listener );
+				    channel.getCloseSetter().set( ( c ) -> {
+					    connections.remove( channel );
+					    listener.onClose( c );
+				    } );
+				    channel.resumeReceives();
+			    }
+		    }, next );
 
-        // In reality, this can just be `/` and apply to all URLs, but a specific suffix
-        // makes it easier to proxy at the web server level
-        addPrefixPath( prefixPath, webSocketProtocolHandshakeHandler );
-    }
+		// In reality, this can just be `/` and apply to all URLs, but a specific suffix
+		// makes it easier to proxy at the web server level
+		addPrefixPath( prefixPath, webSocketProtocolHandshakeHandler );
+	}
 
-    /**
-     * Get all connections
-     */
-    public Set<WebSocketChannel> getConnections() {
-        return connections;
-    }
+	/**
+	 * Get all connections
+	 */
+	public Set<WebSocketChannel> getConnections() {
+		return connections;
+	}
 
-    public void sendMessage( WebSocketChannel channel, String message ) {
-        if ( channel == null || !channel.isOpen() ) {
-            return;
-        }
-        WebSockets.sendText( message, channel, null );
-    }
+	public void sendMessage( WebSocketChannel channel, String message ) {
+		if ( channel == null || !channel.isOpen() ) {
+			return;
+		}
+		WebSockets.sendText( message, channel, null );
+	}
 
-    public void broadcastMessage( String message ) {
-        // Iterate over all open connections and send the message
-        for ( WebSocketChannel channel : connections ) {
-            sendMessage( channel, message );
-        }
-    }
+	public void broadcastMessage( String message ) {
+		// Iterate over all open connections and send the message
+		for ( WebSocketChannel channel : connections ) {
+			sendMessage( channel, message );
+		}
+	}
 
 }
